1ecb4a0941a83c151e98f16dd3c004a71c4fba77
habato <kobatochan22@yahoo.co.jp>
2021-08-04 14:25:42 +0900

    リクエスト投稿機能の実装

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -1,4 +1,27 @@
 class RequestsController < ApplicationController
   def index
+    @room = Room.find(params[:room_id])
+    @requests= @room.requests.includes(:user)
+  end
+
+  def new
+    @request = Request.new
+    @room = Room.find(params[:room_id])
+  end
+
+  def create
+    @room = Room.find(params[:room_id])
+    @request = @room.requests.new(request_params)
+    if @request.save
+      redirect_to room_request_path(@room)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def request_params
+    params.require(:request).permit(:title, :content).merge(user_id: current_user.id)
   end
 end
