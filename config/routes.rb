09d4e3c8de312e2177164a95973f60047d6a6015
habato <kobatochan22@yahoo.co.jp>
2021-08-04 19:21:35 +0900

    リクエスト編集機能のルーティングを設定

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "rooms#index"
   resources :rooms, only: [:new, :create] do
-    resources :requests, only: [:index, :new, :create, :show]
+    resources :requests, only: [:index, :new, :create, :show, :edit, :update]
   end
 end
