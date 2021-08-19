4a5242682c6921decd977f522d5db70617a3b53c
habato <kobatochan22@yahoo.co.jp>
2021-08-19 12:24:03 +0900

    ユーザー詳細ページを作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  get 'users/show'
   devise_for :users
   root to: "rooms#index"
+  resources :users, only: :show
   resources :rooms, only: [:new, :create, :destroy] do
     resources :requests, only: [:index, :new, :create]
   end
