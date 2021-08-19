91f05723acb6abe64baa0a08f20f7cea1a6b86e6
habato <kobatochan22@yahoo.co.jp>
2021-08-19 23:15:46 +0900

    フォロー機能のアソシエーションを記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,4 +17,10 @@ class User < ApplicationRecord
   has_many :requests
   has_many :completions
   has_many :comments
+
+  has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+  has_many :followers, through: :reverse_of_relationships, source: :follower
+
+  has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :followings, through: :relationships, source: :followed
 end
