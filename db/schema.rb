4fef61bb5e9a596b846bb1aea3358f53cd49aef4
habato <kobatochan22@yahoo.co.jp>
2021-08-19 23:11:48 +0900

    relationshipモデルを作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_06_021043) do
+ActiveRecord::Schema.define(version: 2021_08_19_140741) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -50,6 +50,13 @@ ActiveRecord::Schema.define(version: 2021_08_06_021043) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["request_id"], name: "index_completions_on_request_id"
     t.index ["user_id"], name: "index_completions_on_user_id"
+  end
+
+  create_table "relationships", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.integer "follower_id", null: false
+    t.integer "followed_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "requests", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
