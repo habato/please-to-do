5f1aa9a4bb81d08045cd2301b3d56a46409bf211
habato <kobatochan22@yahoo.co.jp>
2021-08-25 20:53:06 +0900

    不要な記述やファイルを削除

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,6 @@ Rails.application.routes.draw do
 
   resources :users, only: [:show] do
     resource :relationships, only: [:create, :destroy]
-    get 'followings' => 'relationships#followings', as: 'followings'
-    get 'followers' => 'relationships#followers', as: 'followers'
     collection do
       get 'search'
     end
